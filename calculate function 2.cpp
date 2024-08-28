1e9a79589449878a7287aaaf72ca7eb7f9383f4f
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-08-28 20:51:49 +0600

    Update calculate function 2.cpp

--- calculate function 2.cpp
+++ calculate function 2.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    int i = 0;
+    int sum = 0;
+
+
+    while (i <= n) {
+        if (i % 2 == 0) {
+            sum += i;  
+        } else {
+            sum -= i; 
+        }
+        i++;  
+    }
+
+    cout << sum; 
+    return 0;
+}
 
